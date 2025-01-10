008a8c21b4ec2e257891df132f80049adecd8e19
yr-nitish <nitishgupta1601@gmail.com>
2025-01-10 17:28:04 +0530

    Initial Commit

---
+++ Java_Course/Module1/src/GettingStarted/MyInitials.java
@@ -0,0 +1,16 @@
+package GettingStarted;
+
+public class MyInitials
+{
+    public static void main(String[] args)
+    {
+        System.out.println("N   N   GGG ");
+        System.out.println("NN  N  G   G");
+        System.out.println("N N N  G    ");
+        System.out.println("N  NN  G GGG");
+        System.out.println("N   N  G   G");
+        System.out.println("N   N  G   G");
+        System.out.println("N   N   GGG ");
+
+    }
+}
---
+++ Java_Course/Module1/src/Variables/Biography.java
@@ -0,0 +1,23 @@
+package Variables;
+
+public class Biography
+{
+    public static void main(String[] args)
+    {
+        String name="Nitish";
+        int age=22;
+        String country="India";
+        String sport="Cricket";
+        int hours=1;
+        String game="Carrom";
+        String subject="Maths";
+        char grade='A';
+
+        System.out.println("My name is " + name + ". I'm " + age + " years old, and I'm from "+country+". ");
+        System.out.println("My favourite sport is "+sport+". I play for "+hours+" hours a day");
+        System.out.println("When I'm tired, I like to play "+ game+".");
+        System.out.println("In school, my favourite subject was "+subject+", I scored a "+grade+" grade.");
+
+
+    }
+}
